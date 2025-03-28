8ab9a7d3356fc38b9bb11965c9e6987ea8eadff4
juliostmendes <mendes.julio@pm.me>
2025-03-28 17:18:58 -0300

    Getting post by id

---
+++ src/main/java/com/mendes/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.mendes.workshopmongo.services;
+
+import com.mendes.workshopmongo.domain.Post;
+import com.mendes.workshopmongo.repositories.PostRepository;
+import com.mendes.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    PostRepository repository;
+
+    public Post findById(String id){
+        Optional<Post> obj = repository.findById(id);
+        if(obj.isEmpty()){
+            throw new ObjectNotFoundException("Object not found");
+        }
+        Post post = obj.get();
+        return  post;
+    }
+}
