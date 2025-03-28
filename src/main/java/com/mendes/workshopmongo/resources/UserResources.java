3aaa97a1791954d4a5990e0af6c0961c43cd58b4
juliostmendes <mendes.julio@pm.me>
2025-03-28 14:57:12 -0300

    User findById impl

--- src/main/java/com/mendes/workshopmongo/resources/UserResources.java
+++ src/main/java/com/mendes/workshopmongo/resources/UserResources.java
@@ -5,6 +5,7 @@ import com.mendes.workshopmongo.dto.UserDTO;
 import com.mendes.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -26,5 +28,11 @@ public class UserResources {
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(UserDTO::new).toList();
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
--- src/main/java/com/mendes/workshopmongo/services/UserService.java
+++ src/main/java/com/mendes/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.mendes.workshopmongo.services;
 
 import com.mendes.workshopmongo.domain.User;
 import com.mendes.workshopmongo.repositories.UserRepository;
+import com.mendes.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,14 @@ public class UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repository.findById(id);
+        if(obj.isEmpty()){
+            throw new ObjectNotFoundException("Object not found");
+        }
+        User user = obj.get();
+        return  user;
     }
 }
