340b6fa5b3374400687e567b6e7c933313cb6bb5
juliostmendes <mendes.julio@pm.me>
2025-03-28 16:04:12 -0300

    Post created

--- src/main/java/com/mendes/workshopmongo/config/Instantiation.java
+++ src/main/java/com/mendes/workshopmongo/config/Instantiation.java
@@ -1,26 +1,41 @@
 package com.mendes.workshopmongo.config;
 
+import com.mendes.workshopmongo.domain.Post;
 import com.mendes.workshopmongo.domain.User;
+import com.mendes.workshopmongo.repositories.PostRepository;
 import com.mendes.workshopmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private PostRepository postRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.deleteAll();
+        userRepository.deleteAll();
+        postRepository.deleteAll();
 
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
         User u1 = new User(null, "Maria Brown", "maria@gmail.com");
         User u2 = new User(null, "Carl Smith", "carlsmith@gmail.com");
         User u3 = new User(null, "William DaFoe", "willdafoe@gmail.com");
 
-        repository.saveAll(Arrays.asList(u1, u2, u3));
+        Post p1 = new Post(null, sdf.parse("2025-01-12") , "How to center a div?", "It look like it's impossible", u1);
+        Post p2 = new Post(null, sdf.parse("2025-01-12") , "Are Brazil dangerous?", "I think that I'll go to Brazil", u1);
+
+        userRepository.saveAll(Arrays.asList(u1, u2, u3));
+        postRepository.saveAll(Arrays.asList(p1, p2));
     }
 }
---
+++ src/main/java/com/mendes/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.mendes.workshopmongo.repositories;
+
+import com.mendes.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
